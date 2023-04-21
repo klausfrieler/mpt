3863fa22e91f2c41715e83852b83d3ddcd77e333
klaus <kgf@omniversum.de>
2023-04-21 22:11:41 +0200

    Changed dictionary import to readr::read_csv, fixed Latvian dictionary.

--- data-raw/mpt-dict.R
+++ data-raw/mpt-dict.R
@@ -1,37 +1,37 @@
-input <- read.csv("data-raw/mpt-dict.csv", stringsAsFactors = FALSE, encoding = "utf-8")
+input <- readr::read_csv("data-raw/mpt-dict.csv")
 names(input)[[1]] <- "key"
 
-russian <- read.csv("data-raw/dict-russian.csv", stringsAsFactors = FALSE, encoding = "utf-8")
+russian <- readr::read_csv("data-raw/dict-russian.csv")
 names(russian)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == russian$key))
 input$RU <- russian$RU
 
-dutch <- read.csv("data-raw/dict-dutch.csv", stringsAsFactors = FALSE, encoding = "utf-8")
+dutch <- readr::read_csv("data-raw/dict-dutch.csv")
 names(dutch)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == dutch$key))
 input$NL <- dutch$NL
 
-italian <- read.csv("data-raw/dict-italian.csv", stringsAsFactors = FALSE, encoding = "utf-8")
+italian <- readr::read_csv("data-raw/dict-italian.csv")
 names(italian)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == italian$key))
 input$IT <- italian$IT
 
-spanish <- read.csv("data-raw/dict-spanish.csv", stringsAsFactors = FALSE, encoding = "utf-8")
+spanish <- readr::read_csv("data-raw/dict-spanish.csv")
 names(spanish)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == spanish$key))
 input$ES <- spanish$ES
 
-formal_german <- read.csv("data-raw/dict-de_formal.csv", stringsAsFactors = FALSE, encoding = "utf-8")
+formal_german <- readr::read_csv("data-raw/dict-de_formal.csv")
 names(formal_german)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == formal_german$key))
 input$DE_F <- formal_german$DE_F
 
-latvian <- read.csv("data-raw/dict-latvian.csv", stringsAsFactors = FALSE)
+latvian <- readr::read_csv("data-raw/dict-latvian.csv")
 names(latvian)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == latvian$key))
